b4cd1a4f59d52b6a56fb58bef11624e1cea6c0ef
Carlos Goncalves <carlos.goncalves@neclab.eu>
2016-03-20 22:05:39 +0100

    Output log when compute node is back online
    The network interface of the compute node is set to down for 180 seconds, and
    then brought up back again. While it's unreachable we cannot call out the
    COMPUTE_IP:disable_network.log
    
    Change-Id: Iaa90339093c8fda88d883b688a4c8495e78414b7
    Signed-off-by: Carlos Goncalves <carlos.goncalves@neclab.eu>

--- tests/run.sh
+++ tests/run.sh
@@ -161,8 +161,6 @@ cleanup() {
     stop_monitor
     stop_inspector
     stop_consumer
-    ssh $ssh_opts_cpu "heat-admin@$COMPUTE_IP" \
-        "[ -e disable_network.log ] && cat disable_network.log"
 
     python ./nova_force_down.py "$COMPUTE_HOST" --unset
     sleep 1
@@ -178,6 +176,8 @@ cleanup() {
     #TODO: add host status check via nova admin api
     echo "waiting disabled compute host back to be enabled..."
     sleep 180
+    ssh $ssh_opts_cpu "heat-admin@$COMPUTE_IP" \
+        "[ -e disable_network.log ] && cat disable_network.log"
 }
 
 
