9d11cc17424674f616fa6b9a408a4b1a14577b5f
dongwenjuan <dong.wenjuan@zte.com.cn>
2016-07-28 19:35:36 +0800

    test scenario support fuel installer
    This patch extends support for fuel OPNFV installer for the test
    scenario.
    
    JIRA: DOCTOR-62
    
    Change-Id: I09746b0e64b3999363db53c318027b81c471e2c4
    Signed-off-by: dongwenjuan <dong.wenjuan@zte.com.cn>

--- tests/run.sh
+++ tests/run.sh
@@ -25,8 +25,8 @@ DOCTOR_PROJECT=doctor
 #TODO: change back to `_member_` when JIRA DOCTOR-55 is done
 DOCTOR_ROLE=admin
 
-SUPPORTED_INSTALLER_TYPES="apex local"
-INSTALLER_TYPE=${INSTALLER_TYPE:-apex}
+SUPPORTED_INSTALLER_TYPES="apex fuel local"
+INSTALLER_TYPE=${INSTALLER_TYPE:-local}
 INSTALLER_IP=${INSTALLER_IP:-none}
 
 ssh_opts="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
@@ -57,13 +57,23 @@ get_compute_host_info() {
              "source stackrc; \
              nova show $COMPUTE_HOST \
              | awk '/ ctlplane network /{print \$5}'")
+    elif [[ "$INSTALLER_TYPE" == "fuel" ]] ; then
+        COMPUTE_USER=${COMPUTE_USER:-root}
+        if [[ "$INSTALLER_IP" == "none" ]] ; then
+            instack_mac=$(sudo virsh domiflist fuel-opnfv | awk '/pxebr/{print $5}')
+            INSTALLER_IP=$(/usr/sbin/arp -e | grep ${instack_mac} | awk '{print $1}')
+        fi
+        node_id=$(echo $COMPUTE_HOST | cut -d "-" -f 2)
+        COMPUTE_IP=$(sshpass -p r00tme ssh 2>/dev/null $ssh_options root@${INSTALLER_IP} \
+             "fuel node|awk -F '|' -v id=$node_id '{if (\$1 == id) print \$5}' |xargs")
     elif [[ "$INSTALLER_TYPE" == "local" ]] ; then
         COMPUTE_USER=${COMPUTE_USER:-$(whoami)}
         COMPUTE_IP=$(getent hosts "$COMPUTE_HOST" | awk '{ print $1 }')
-        if [[ -z "$COMPUTE_IP" ]]; then
-            echo "ERROR: Could not resolve $COMPUTE_HOST. Either manually set COMPUTE_IP or enable DNS resolution."
-            exit 1
-        fi
+    fi
+
+    if [[ -z "$COMPUTE_IP" ]]; then
+        echo "ERROR: Could not resolve $COMPUTE_HOST. Either manually set COMPUTE_IP or enable DNS resolution."
+        exit 1
     fi
 
     # verify connectivity to target compute host
@@ -80,12 +90,16 @@ prepare_compute_ssh() {
     # get ssh key from installer node
     if [[ "$INSTALLER_TYPE" == "apex" ]] ; then
         sudo scp $ssh_opts root@"$INSTALLER_IP":/home/stack/.ssh/id_rsa instack_key
-        sudo chown $(whoami):$(whoami) instack_key
-        chmod 400 instack_key
-        ssh_opts_cpu+=" -i instack_key"
+    elif [[ "$INSTALLER_TYPE" == "fuel" ]] ; then
+        sshpass -p r00tme ssh 2>/dev/null $ssh_options root@${INSTALLER_IP} \
+            "cat /root/.ssh/id_rsa" > instack_key
     elif [[ "$INSTALLER_TYPE" == "local" ]] ; then
         echo "INSTALLER_TYPE set to 'local'. Assuming SSH keys already exchanged with $COMPUTE_HOST"
     fi
+
+    sudo chown $(whoami):$(whoami) instack_key
+    chmod 400 instack_key
+    ssh_opts_cpu+=" -i instack_key"
 
     # verify ssh to target compute host
     ssh $ssh_opts_cpu "$COMPUTE_USER@$COMPUTE_IP" 'exit'
