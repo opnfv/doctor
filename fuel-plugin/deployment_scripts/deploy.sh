e54fac980ba77b6a4f6ad95eb46394f2d3660b02
dongwenjuan <dong.wenjuan@zte.com.cn>
2016-08-10 13:23:51 +0800

    add doctor fuel plugin
    Doctor needs to have a ceilometer configuration set in order to work
    for the test scenario, we should use a plugin to deliver your
    configuration change.
    
    JIRA: DOCTOR-68
    
    Change-Id: I1405998252e3c0b317cd319fe6872195240878c1
    Signed-off-by: dongwenjuan <dong.wenjuan@zte.com.cn>

---
+++ fuel-plugin/deployment_scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Fuel Plugin - OPNFV Fault Managment (Doctor)
+#
+# https://wiki.opnfv.org/display/doctor
+#
+# Version 9.0
+
+CEILOMETER_CONF_DIR=/etc/ceilometer
+
+
+if [ -e $CEILOMETER_CONF_DIR/event_pipeline.yaml ]; then
+    if ! grep -q '^ *- notifier://?topic=alarm.all$' $CEILOMETER_CONF_DIR/event_pipeline.yaml; then
+        sed -i 's|- notifier://|- notifier://?topic=alarm.all|' $CEILOMETER_CONF_DIR/event_pipeline.yaml
+        service ceilometer-agent-notification restart
+    fi
+else
+    echo "doctor failed: ceilometer file is not exist"
+    exit 1
+fi
+
+exit 0
