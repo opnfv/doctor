d719fde412991d025616e8245499a16dbea4b190
Tomi Juvonen <tomi.juvonen@nokia.com>
2016-12-15 10:10:07 +0200

    Change role of doctor user to _member_
    Change role of doctor user to _member_
    Do needful changes to API calls
    Add admin a role to project
    Add Fuel support
    
    JIRA: DOCTOR-80
    
    Change-Id: Ic7f88b9ceb6c4fc90e5e985ecfbc50c907b2367e
    Signed-off-by: Tomi Juvonen <tomi.juvonen@nokia.com>

--- tests/run.sh
+++ tests/run.sh
@@ -25,21 +25,21 @@ CONSUMER_PORT=12346
 DOCTOR_USER=doctor
 DOCTOR_PW=doctor
 DOCTOR_PROJECT=doctor
-#TODO: change back to `_member_` when JIRA DOCTOR-55 is done
-DOCTOR_ROLE=admin
+DOCTOR_ROLE=_member_
 PROFILER_TYPE=${PROFILER_TYPE:-none}
 
 TOP_DIR=$(cd $(dirname "$0") && pwd)
 
 as_doctor_user="--os-username $DOCTOR_USER --os-password $DOCTOR_PW
                 --os-tenant-name $DOCTOR_PROJECT"
+as_admin_user="--os-username admin --os-tenant-name $DOCTOR_PROJECT"
 
 
 # Functions
 
 get_compute_host_info() {
-    # get computer host info which VM boot in
-    COMPUTE_HOST=$(openstack $as_doctor_user server show $VM_NAME |
+    # get computer host info which VM boot in as admin user
+    COMPUTE_HOST=$(openstack $as_admin_user server show $VM_NAME |
                    grep "OS-EXT-SRV-ATTR:host" | awk '{ print $4 }')
     compute_host_in_undercloud=${COMPUTE_HOST%%.*}
     die_if_not_set $LINENO COMPUTE_HOST "Failed to get compute hostname"
@@ -112,6 +112,10 @@ create_test_user() {
     | grep -q " $DOCTOR_ROLE " || {
         openstack role add "$DOCTOR_ROLE" --user "$DOCTOR_USER" \
                            --project "$DOCTOR_PROJECT"
+    }
+    openstack user role list admin --project "$DOCTOR_PROJECT" \
+    | grep -q " admin " || {
+        openstack role add admin --user admin --project "$DOCTOR_PROJECT"
     }
 }
 
@@ -342,6 +346,7 @@ cleanup() {
     fi
     openstack role remove "$DOCTOR_ROLE" --user "$DOCTOR_USER" \
                               --project "$DOCTOR_PROJECT"
+    openstack role remove admin --user admin --project "$DOCTOR_PROJECT"
     openstack project delete "$DOCTOR_PROJECT"
     openstack user delete "$DOCTOR_USER"
 
