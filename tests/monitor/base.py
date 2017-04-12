2794692ecd9c331d752175ddcf3ec61f9be012d8
dongwenjuan <dong.wenjuan@zte.com.cn>
2017-04-12 10:46:39 +0800

    refactor the monitor
    JIRA: DOCTOR-99
    
    Change-Id: I1831d329ffe80435532678fcb23d54f310422ce8
    Signed-off-by: dongwenjuan <dong.wenjuan@zte.com.cn>

---
+++ tests/monitor/base.py
@@ -0,0 +1,31 @@
+##############################################################################
+# Copyright (c) 2017 ZTE Corporation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+from oslo_utils import importutils
+
+
+_monitor_name_class_mapping = {
+    'sample': 'monitor.sample.SampleMonitor'
+}
+
+def get_monitor(conf, inspector_url):
+    monitor_class = _monitor_name_class_mapping.get(conf.monitor.type)
+    return importutils.import_object(monitor_class, conf, inspector_url)
+
+
+class Monitor(object):
+
+    def __init__(self, conf, inspector_url):
+        self.conf = conf
+        self.inspector_url = inspector_url
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
