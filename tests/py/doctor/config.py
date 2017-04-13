860d2bf1067eb17fdc23458cac3ac16853fe0482
dongwenjuan <dong.wenjuan@zte.com.cn>
2017-04-13 09:09:41 +0800

    [WIP]add tox
    Change-Id: Ife3255c4fe2d170c5d1b8354aaab24d5b1e3bc64
    Signed-off-by: dongwenjuan <dong.wenjuan@zte.com.cn>

---
+++ tests/py/doctor/config.py
@@ -0,0 +1,25 @@
+##############################################################################
+# Copyright (c) 2017 ZTE Corporation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+from oslo_config import cfg
+
+
+def list_opts():
+    return []
+
+
+def prepare_conf(conf=None):
+    if conf is None:
+        conf = cfg.ConfigOpts()
+
+    for group, options in list_opts():
+        conf.register_opts(list(options),
+                           group=None if group == 'DEFAULT' else group)
+
+    return conf
