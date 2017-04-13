1e7db5d9aa7e106d0acac650a58a31135c1f8267
dongwenjuan <dong.wenjuan@zte.com.cn>
2017-04-13 09:09:41 +0800

    add framework and tox
    Change-Id: Ife3255c4fe2d170c5d1b8354aaab24d5b1e3bc64
    Signed-off-by: dongwenjuan <dong.wenjuan@zte.com.cn>

---
+++ doctor/main.py
@@ -0,0 +1,55 @@
+##############################################################################
+# Copyright (c) 2017 ZTE Corporation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+import sys
+
+import config
+import logger as doctor_log
+
+
+LOG = doctor_log.Logger(__name__).getLogger()
+
+
+class DoctorTest(object):
+
+    def __init__(self, conf):
+        self.conf = conf
+
+    def run(self):
+        """run doctor test"""
+        try:
+            LOG.info('doctor test starting.......')
+            # prepare the cloud env
+
+            # preparing VM image...
+
+            # creating test user...
+
+            # creating VM...
+
+            # creating alarm...
+
+            # starting doctor sample components...
+
+            # injecting host failure...
+
+            # verify the test results
+        except Exception as e:
+            LOG.error('doctor test failed: %s ', e)
+
+
+def main():
+    """doctor main"""
+    conf = config.prepare_conf()
+
+    doctor = DoctorTest(conf)
+    doctor.run()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
