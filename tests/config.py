38ebd0f056f604e14cb1b07f938cfd792b34c3a6
dongwenjuan <dong.wenjuan@zte.com.cn>
2017-05-10 15:21:38 +0800

    add create instances
    JIRA: DOCTOR-93
    
    Change-Id: I7645c1cc0c76ff86b9fbcc01c19038469fc9b9b4
    Signed-off-by: dongwenjuan <dong.wenjuan@zte.com.cn>

--- tests/config.py
+++ tests/config.py
@@ -6,16 +6,22 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
+import itertools
 from oslo_config import cfg
 
 import image
+import instance
+import network
 import os_clients
 
 
 def list_opts():
     return [
-        ('os_clients', os_clients.OPTS),
         ('image', image.IMAGE_OPTS),
+        ('DEFAULT', itertools.chain(
+            os_clients.OPTS,
+            network.OPTS,
+            instance.OPTS))
     ]
 
 
