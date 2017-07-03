795ff29122994fc66f45e1eb37c8650eec03f554
dongwenjuan <dong.wenjuan@zte.com.cn>
2017-07-03 10:03:35 +0800

    refactor sample inspector
    JIRA: DOCTOR-98
    
    Change-Id: I7110b680d0a0c64b1b2666abcea183e73373dca7
    Signed-off-by: dongwenjuan <dong.wenjuan@zte.com.cn>

---
+++ tests/inspector/__init__.py
@@ -0,0 +1,37 @@
+#############################################################################
+# Copyright (c) 2017 ZTE Corporation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+import os
+
+from oslo_config import cfg
+from oslo_utils import importutils
+
+OPTS = [
+    cfg.StrOpt('type',
+               default=os.environ.get('INSPECTOR_TYPE', 'sample'),
+               choices=['sample', 'congress', 'vitrage'],
+               help='the component of doctor inspector',
+               required=True),
+    cfg.StrOpt('ip',
+               default='127.0.0.1',
+               help='the ip of default inspector',
+               required=False),
+    cfg.StrOpt('port',
+               default='12345',
+               help='the port of default for inspector',
+               required=False),
+]
+
+
+_inspector_name_class_mapping = {
+    'sample': 'inspector.sample.SampleInspector',
+}
+
+def get_inspector(conf, log):
+    inspector_class = _inspector_name_class_mapping[conf.inspector.type]
+    return importutils.import_object(inspector_class, conf, log)
