d737adf895013a3edbd5d67d06f182eeda7b52e9
dongwenjuan <dong.wenjuan@zte.com.cn>
2017-07-03 10:03:35 +0800

    refactor sample inspector
    JIRA: DOCTOR-98
    
    Change-Id: I7110b680d0a0c64b1b2666abcea183e73373dca7
    Signed-off-by: dongwenjuan <dong.wenjuan@zte.com.cn>

--- tests/main.py
+++ tests/main.py
@@ -10,6 +10,7 @@ import sys
 
 import config
 from image import Image
+from inspector import get_inspector
 import logger as doctor_log
 from user import User
 
@@ -23,6 +24,7 @@ class DoctorTest(object):
         self.conf = conf
         self.image = Image(self.conf, LOG)
         self.user = User(self.conf, LOG)
+        self.inspector = get_inspector(self.conf, LOG)
 
     def setup(self):
         # prepare the cloud env
@@ -33,6 +35,9 @@ class DoctorTest(object):
         # creating test user...
         self.user.create()
         self.user.update_quota()
+
+        # starting doctor sample components...
+        self.inspector.start()
 
     def run(self):
         """run doctor test"""
@@ -54,6 +59,7 @@ class DoctorTest(object):
     def cleanup(self):
         self.image.delete()
         self.user.delete()
+        self.inspector.stop()
 
 
 def main():
