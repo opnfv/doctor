450908326fd250f1fa9942d120d0c6e9aa1aafa9
dongwenjuan <dong.wenjuan@zte.com.cn>
2017-07-17 08:54:22 +0800

    refactor local installer
    JIRA: DOCTOR-100
    
    Change-Id: I1e2c53f7a716156e35386d42c0df32a228a78fab
    Signed-off-by: dongwenjuan <dong.wenjuan@zte.com.cn>

---
+++ tests/installer/__init__.py
@@ -0,0 +1,37 @@
+##############################################################################
+# Copyright (c) 2017 ZTE Corporation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+import os
+
+from oslo_config import cfg
+from oslo_utils import importutils
+
+OPTS = [
+    cfg.StrOpt('type',
+               default=os.environ.get('INSTALLER_TYPE', 'local'),
+               choices=['local'],
+               help='the type of installer',
+               required=True),
+    cfg.StrOpt('ip',
+               default=os.environ.get('INSTALLER_IP', '127.0.0.1'),
+               help='the ip of installer'),
+    cfg.StrOpt('username',
+               default='root',
+               help='the user name for login installer server',
+               required=True),
+]
+
+
+_installer_name_class_mapping = {
+    'local': 'installer.local.LocalInstaller'
+}
+
+
+def get_installer(conf, log):
+    installer_class = _installer_name_class_mapping[conf.installer.type]
+    return importutils.import_object(installer_class, conf, log)
