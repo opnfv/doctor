48aca6c2875ef7ff8c658d70beb729527b88958d
dongwenjuan <dong.wenjuan@zte.com.cn>
2017-08-17 08:57:27 +0800

    fix some bugs in monitor
    1.send bytes data to the socket
    see https://docs.python.org/3/library/socket.html
    2.fix a endless loop
    3.Received all the SOCK_RAW from multiple compute nodes
    Change-Id: I880e713266347d8836cec45ebf9a500bb7c813f4
    Signed-off-by: dongwenjuan <dong.wenjuan@zte.com.cn>

--- tests/monitor/sample.py
+++ tests/monitor/sample.py
@@ -78,7 +78,7 @@ class SampleMonitor(BaseMonitor):
 class Pinger(Thread):
     interval = 0.1  # second
     timeout = 0.1   # second
-    ICMP_ECHO_MESSAGE = '\x08\x00\xf7\xff\x00\x00\x00\x00'
+    ICMP_ECHO_MESSAGE = bytes([0x08, 0x00, 0xf7, 0xff, 0x00, 0x00, 0x00, 0x00])
 
     def __init__(self, host_name, host_ip, monitor, log):
         Thread.__init__(self)
@@ -89,31 +89,36 @@ class Pinger(Thread):
         self._stopped = False
 
     def run(self):
-        while True:
-            if self._stopped:
-                return
-            self._run()
-            time.sleep(self.interval)
-
-    def stop(self):
-        self.log.info("Stopping Pinger host_name(%s), host_ip(%s)"
-                      % (self.hostname, self.ip_addr))
-        self._stopped = True
-
-    def _run(self):
         self.log.info("Starting Pinger host_name(%s), host_ip(%s)"
                       % (self.hostname, self.ip_addr))
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_RAW,
                              socket.IPPROTO_ICMP)
         sock.settimeout(self.timeout)
+        count = 0
         while True:
+            if self._stopped:
+                return
             try:
-                sock.sendto(self.ICMP_ECHO_MESSAGE.encode(), (self.ip_addr, 0))
-                sock.recv(4096)
+                sock.sendto(self.ICMP_ECHO_MESSAGE, (self.ip_addr, 0))
+                data, address = sock.recvfrom(4096)
+                # It'll received all the SOCK_RAW from multiple compute nodes
+                if address != (self.ip_addr, 0):
+                    count += 1
+                    if count > 10:
+                        self.log.info("doctor monitor detected at %s" % time.time())
+                        return
+                else:
+                    count = 0
             except socket.timeout:
                 self.log.info("doctor monitor detected at %s" % time.time())
                 self.monitor.report_error(self.hostname)
                 self.log.info("ping timeout, quit monitoring...")
                 self._stopped = True
                 return
+            time.sleep(self.interval)
+
+    def stop(self):
+        self.log.info("Stopping Pinger host_name(%s), host_ip(%s)"
+                      % (self.hostname, self.ip_addr))
+        self._stopped = True
