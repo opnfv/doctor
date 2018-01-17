67942714b16ff0d8d1b3b7037a1f5dfb5359d558
Tomi Juvonen <tomi.juvonen@nokia.com>
2018-01-17 10:20:44 +0200

    Add setup and cleanup for maintenance test
    JIRA: DOCTOR-119
    
    Change-Id: Idb95e1391e8f9094f68e2c7bc79fd2d7e01af9de
    Signed-off-by: Tomi Juvonen <tomi.juvonen@nokia.com>

--- doctor_tests/installer/common/restore_config.py
+++ doctor_tests/installer/common/restore_config.py
@@ -24,4 +24,32 @@ def restore_ep_config():
     return
 
 
+def restore_ed_config():
+
+    ed_file = '/etc/ceilometer/event_definitions.yaml'
+    ed_file_bak = '/etc/ceilometer/event_definitions.bak'
+
+    if not os.path.isfile(ed_file_bak):
+        print("Bak_file doesn't exist: %s." % ed_file_bak)
+    else:
+        print('restore: %s' % ed_file)
+        shutil.copyfile(ed_file_bak, ed_file)
+        os.remove(ed_file_bak)
+    return
+
+
+def restore_cpu_allocation_ratio():
+    nova_file = '/etc/nova/nova.conf'
+    nova_file_bak = '/etc/nova/nova.bak'
+
+    if not os.path.isfile(nova_file_bak):
+        print('Bak_file:%s does not exist.' % nova_file_bak)
+    else:
+        print('restore: %s' % nova_file)
+        shutil.copyfile(nova_file_bak, nova_file)
+        os.remove(nova_file_bak)
+    return
+
 restore_ep_config()
+restore_ed_config()
+restore_cpu_allocation_ratio()
