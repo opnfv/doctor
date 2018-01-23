c942be45f70ec9f1a7dc78a58ba5ebd248eaf4ae
dongwenjuan <dong.wenjuan@zte.com.cn>
2018-01-23 11:41:53 +0800

    [Don't merge] test for CI
    Change-Id: I9b7d728ee6cc0e233c94c8df538d6ac70de4259a
    Signed-off-by: dongwenjuan <dong.wenjuan@zte.com.cn>

--- doctor_tests/instance.py
+++ doctor_tests/instance.py
@@ -5,6 +5,7 @@
 # are made available under the terms of the Apache License, Version 2.0
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
+# test
 ##############################################################################
 import os
 import time
