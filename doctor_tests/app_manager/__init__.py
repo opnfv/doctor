8855c1612dd05dea609294499664fb2f56f4adae
Tomi Juvonen <tomi.juvonen@nokia.com>
2018-03-08 07:13:36 +0200

    Add maintenance test code
      -Add sample admin_tool
      -Add sample app_manager
      -Modify sample inspector
    
    JIRA: DOCTOR-106
    
    Change-Id: I52cffecaa88452ce5e7cc6487534c88fcfd378ad
    Signed-off-by: Tomi Juvonen <tomi.juvonen@nokia.com>

---
+++ doctor_tests/app_manager/__init__.py
@@ -0,0 +1,38 @@
+##############################################################################
+# Copyright (c) 2018 Nokia Corporation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+from oslo_config import cfg
+from oslo_utils import importutils
+
+
+OPTS = [
+    cfg.StrOpt('type',
+               default='sample',
+               choices=['sample'],
+               help='the component of doctor app manager',
+               required=True),
+    cfg.StrOpt('ip',
+               default='127.0.0.1',
+               help='the ip of app manager',
+               required=True),
+    cfg.IntOpt('port',
+               default='12348',
+               help='the port of doctor app manager',
+               required=True),
+]
+
+
+_app_manager_name_class_mapping = {
+    'sample': 'doctor_tests.app_manager.sample.SampleAppManager'
+}
+
+
+def get_app_manager(stack, conf, log):
+    app_manager_class = (
+        _app_manager_name_class_mapping.get(conf.app_manager.type))
+    return importutils.import_object(app_manager_class, stack, conf, log)
