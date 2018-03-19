6bd4e026fca92b281382a3f6b46f7ab7eeb31f45
dongwenjuan <dong.wenjuan@zte.com.cn>
2018-03-19 17:12:03 +0800

    support MCP installer
    1.support MCP installer
    2.optimize the installer code
    
    JIRA: DOCTOR-121
    
    Change-Id: I8675c2652944575a8f73d0d1e2dafaad5a3e88e3
    Signed-off-by: dongwenjuan <dong.wenjuan@zte.com.cn>

--- doctor_tests/installer/mcp.py
+++ doctor_tests/installer/mcp.py
@@ -1,33 +1,34 @@
 ##############################################################################
-# Copyright (c) 2017 ZTE Corporation and others.
+# Copyright (c) 2018 ZTE Corporation and others.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
+from os.path import isfile
+
 from doctor_tests.common.utils import SSHClient
 from doctor_tests.installer.base import BaseInstaller
 
 
-class ApexInstaller(BaseInstaller):
-    node_user_name = 'heat-admin'
+class McpInstaller(BaseInstaller):
+    node_user_name = 'ubuntu'
     cm_set_script = 'set_ceilometer.py'
     cm_restore_script = 'restore_ceilometer.py'
 
     def __init__(self, conf, log):
-        super(ApexInstaller, self).__init__(conf, log)
+        super(McpInstaller, self).__init__(conf, log)
+        self.key_file = self.get_ssh_key_from_installer()
         self.client = SSHClient(self.conf.installer.ip,
-                                self.conf.installer.username,
-                                look_for_keys=True)
-        self.key_file = None
+                                self.node_user_name,
+                                key_filename=self.key_file)
         self.controllers = list()
         self.controller_clients = list()
 
     def setup(self):
-        self.log.info('Setup Apex installer start......')
+        self.log.info('Setup MCP installer start......')
 
-        self.key_file = self.get_ssh_key_from_installer()
         self.controllers = self.get_controller_ips()
         self.create_flavor()
         self.set_apply_patches()
@@ -39,35 +40,37 @@ class ApexInstaller(BaseInstaller):
             server.terminate()
 
     def get_ssh_key_from_installer(self):
-        key_path = '/home/stack/.ssh/id_rsa'
-        return self._get_ssh_key(self.client, key_path)
+        self.log.info('Get SSH keys from MCP......')
+
+        # Assuming mcp.rsa is already mapped to functest container
+        # if not, only the test runs on jumphost can get the ssh_key
+        # default in path /var/lib/opnfv/mcp.rsa
+        ssh_key = '/root/.ssh/id_rsa'
+        mcp_key = '/var/lib/opnfv/mcp.rsa'
+        return ssh_key if isfile(ssh_key) else mcp_key
 
     def get_controller_ips(self):
-        self.log.info('Get controller ips from Apex installer......')
+        self.log.info('Get controller ips from Mcp installer......')
 
-        command = "source stackrc; " \
-                  "nova list | grep ' overcloud-controller-[0-9] ' " \
-                  "| sed -e 's/^.*ctlplane=//' |awk '{print $1}'"
+        command = "sudo salt --out yaml 'ctl*' " \
+                  "pillar.get _param:openstack_control_address |" \
+                  "awk '{print $2}'"
         controllers = self._run_cmd_remote(self.client, command)
-        self.log.info('Get controller_ips:%s from Apex installer'
+        self.log.info('Get controller_ips:%s from Mcp installer'
                       % controllers)
         return controllers
 
     def get_host_ip_from_hostname(self, hostname):
-        self.log.info('Get host ip by hostname=%s from Apex installer......'
-                      % hostname)
-
-        hostname_in_undercloud = hostname.split('.')[0]
-        command = "source stackrc; nova show %s | awk '/ ctlplane network /{print $5}'" % (hostname_in_undercloud)   # noqa
+        command = "sudo salt --out yaml '%s*' " \
+                  "pillar.get _param:single_address |" \
+                  "awk '{print $2}'" % hostname
         host_ips = self._run_cmd_remote(self.client, command)
         return host_ips[0]
 
     def set_apply_patches(self):
         self.log.info('Set apply patches start......')
 
-        restart_cm_cmd = 'sudo systemctl restart ' \
-                         'openstack-ceilometer-notification.service'
-
+        restart_cm_cmd = 'sudo service ceilometer-agent-notification restart'
         for node_ip in self.controllers:
             client = SSHClient(node_ip, self.node_user_name,
                                key_filename=self.key_file)
@@ -79,9 +82,7 @@ class ApexInstaller(BaseInstaller):
     def restore_apply_patches(self):
         self.log.info('restore apply patches start......')
 
-        restart_cm_cmd = 'sudo systemctl restart ' \
-                         'openstack-ceilometer-notification.service'
-
+        restart_cm_cmd = 'sudo service ceilometer-agent-notification restart'
         for client in self.controller_clients:
             self._run_apply_patches(client,
                                     restart_cm_cmd,
